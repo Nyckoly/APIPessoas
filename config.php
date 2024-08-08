dcdb54832c19e62ffe6d8d35d80726f9973c2d84
NYCKOLY KETTLYN SIQUEIRA DA SILVA <nyckoly.kssilva@senacsp.edu.br>
2024-08-08 16:33:04 -0300

    Feito configuração e conexão com o banco de dados.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_servidor = 'localhost';
+$db_database = 'apipessoas';
+$db_usuario = 'nyckoly';
+$db_pwd = '12345678';
+
+$pdo = new PDO('mysql:host='.$db_servidor.';dbname='.$db_database,$db_usuario,$db_pwd);
+
+$array[] =[
+    'error' => "",
+    'result' => []
+]
+
+?>
